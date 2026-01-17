9c276efae868a28e76519196639d1cb6eec9976b
mrqueen914-boop <mrqueen914@gmail.com>
2026-01-17 18:35:49 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-
+function toggleMenu() {
+  const menu = document.getElementById("sideMenu");
+  if (menu.style.left === "0px") {
+    menu.style.left = "-260px";
+  } else {
+    menu.style.left = "0px";
+  }
+}
